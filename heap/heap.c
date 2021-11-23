3809df24389efa2c9335b592c6ae74743bdb8660
Palavecino <npalavecino@ombutech.net>
2021-11-23 08:21:36 -0300

    completo algunas primitivas de heap

--- heap/heap.c
+++ heap/heap.c
@@ -4,6 +4,10 @@
 #include <string.h> //strcpy(), strcmp()
 
 
+const size_t CAPACIDAD_INICIAL = 20;
+const size_t FACTOR_CARGA = 2;
+
+
 struct heap {
 	void** arr;
 	size_t cantidad;
@@ -12,8 +16,115 @@ struct heap {
 };
 
 
+// AUXILIAR
+void** copiar_arreglo(void** datos, size_t vieja_capacidad, size_t nueva_capacidad) {
+
+    void** nuevo_arreglo = malloc(nueva_capacidad * sizeof(void*));
+    if (!nuevo_arreglo) return NULL;
+
+    for (size_t i=0; i<vieja_capacidad; i++) {
+        nuevo_arreglo[i] = datos[i];
+    }
+
+    for (size_t i=vieja_capacidad; i<nueva_capacidad; i++) {
+        nuevo_arreglo[i] = NULL;
+    }
+
+    return nuevo_arreglo;
+}
 
 
+// AUXILIAR
+heap_t* heap_crear_2(void** datos, size_t n, cmp_func_t cmp) {
+
+    if (!cmp) return NULL;
+
+    heap_t* heap = malloc(sizeof(heap_t));
+    if (!heap) return NULL;
+
+    if (!datos || n == 0) {
+        void** arr = malloc(n * sizeof(void*));
+        if (!arr) return NULL;
+
+        for (size_t i=0; i<n; i++) {
+            arr[i] = NULL;
+        }
+ 
+        heap->arr = arr;
+        heap->cantidad = 0;
+        heap->capacidad = n;
+    } else {
+        size_t nueva_capacidad = FACTOR_CARGA * n;
+
+        heap->arr = copiar_arreglo(datos, n, nueva_capacidad);
+        if (!heap->arr) return NULL;
+
+        //heapify(heap->arr); // ordenamos en forma de heap la copia del arreglo que nos pasaron
+
+        heap->cantidad = n;
+        heap->capacidad = nueva_capacidad;
+    }
+
+    heap->cmp = cmp;
+
+    return heap;
+} 
 
 
+heap_t *heap_crear(cmp_func_t cmp) {
+    return heap_crear_2(NULL, CAPACIDAD_INICIAL, cmp);
+}
 
+
+heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
+    return heap_crear_2(arreglo, n, cmp);
+}
+
+
+size_t heap_cantidad(const heap_t *heap) {
+    if (!heap || !heap->arr) return 0;
+    return heap->cantidad;
+}
+
+
+bool heap_esta_vacio(const heap_t *heap) {
+    return heap_cantidad(heap) == 0;
+}
+
+
+void *heap_ver_max(const heap_t *heap) {
+    if (heap_esta_vacio(heap)) return NULL;
+    return heap->arr[0];
+}
+
+
+// TERMINAR
+bool heap_encolar(heap_t *heap, void *elem) {
+
+}
+
+
+// TERMINAR
+void *heap_desencolar(heap_t *heap) {
+    if (heap_esta_vacio(heap)) return NULL;
+    void* maximo = heap->arr[0];
+
+    // desencolar...
+
+    return maximo;
+}
+
+
+// TERMINAR
+void heap_destruir(heap_t *heap, void (*destruir_elemento)(void *e)) {
+
+}
+
+
+// TERMINAR
+void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp) {
+
+    if (!cmp || !elementos || cant == 0) return;
+
+    // ordenamiento...
+}
