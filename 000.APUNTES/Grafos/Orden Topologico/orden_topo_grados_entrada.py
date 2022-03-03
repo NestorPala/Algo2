1490e5fb0fc8b3dee9fb478f7806f42b602d3772
NestorPala <npalavecino@fi.uba.ar>
2022-03-03 15:10:45 -0300

    agrego codigo mst

--- 000.APUNTES/Grafos/Orden Topologico/orden_topo_grados_entrada.py
+++ 000.APUNTES/Grafos/Orden Topologico/orden_topo_grados_entrada.py
@@ -1,3 +1,12 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+ 
+
+##########################################################
+
+
 from grafo import Grafo
 from collections import deque
 
