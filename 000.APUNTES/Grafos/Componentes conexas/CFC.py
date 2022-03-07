d2530a9e05bdd52ee1c8b62a419df241c420f72b
NestorPala <npalavecino@fi.uba.ar>
2022-03-07 14:17:32 -0300

    cambios de carpetas en grafos

---
+++ 000.APUNTES/Grafos/Componentes conexas/CFC.py
@@ -0,0 +1,15 @@
+# CFC - PENDIENTE
+
+
+from grafo import Grafo
+
+
+def cfc(grafo: Grafo) -> list:
+    pass
+
+
+def main() -> None:
+    pass
+
+
+main()
--- 000.APUNTES/Grafos/MST/Prim/prim.py
+++ 000.APUNTES/Grafos/MST/Prim/prim.py
@@ -1,6 +1,7 @@
 import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)
 sys.path.append(parentdir)
  
 
